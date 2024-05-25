07e27e92acf5c530f98c863afbbbacc684053dd7
hongyu6666 <137482748+hongyu6666@users.noreply.github.com>
2024-05-25 11:05:24 +0800

    Update device.h

--- device.h
+++ device.h
@@ -1,27 +1,36 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
 #pragma once
 #include<bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <cstdlib>
 using namespace std;
-#define deviceNum 3
 
-typedef struct {
+#define DeviceNum 6
+#define MOUSE 0
+#define SCREEN 1
+#define KEYBOARD 2
+#define PRINTER 3
+#define TESTDEVICE1 4
+#define TESTDEVICE2 5
+
+struct Device{
     string name;//设备名称
     int *data;//等待队列
     int size;//等待队列大小，为0说明设备空闲
-} table;
+    int num;//设备可用数量
+} ;
 
-table Device_Table[deviceNum];//设备表
-string deviceName[] = { "mouse", "screen",  "keyboard" };
+Device Device_Table[DeviceNum];//设备表
+string deviceName[] = { "mouse", "screen",  "keyboard" ,"printer","test1","test2"};
+int deviceNum[] ={1,2,1,2,3,4};
 
-void device_init();
+void init_device();
 
-int device_findNum(string device);//从设备名称获得设备号
+// int device_findNum(string device);//从设备名称获得设备号
 
-void device_showDevice();
+void show_Device();
 
-bool device_acquire(int pid, int device);//device：请求设备号
+bool acquire_request(int pid, int device);//device：请求设备号
 
-bool device_release(int pid, int device);//device：释放设备号，-1代表释放进程占用的全部设备
+bool release_device(int pid, int device);//device：释放设备号 -1代表释放进程占用的全部设备
