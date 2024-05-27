ee1146055290dbfe8a749dba2a5cd4a118c9b8aa
zhaoshuai123123 <92283710+zhaoshuai123123@users.noreply.github.com>
2024-05-27 20:55:12 +0800

    Update ui.cpp

--- osTest/ui.cpp
+++ osTest/ui.cpp
@@ -1,16 +1,5 @@
 #include "ui.h"
 
-
-
-
-
-
-
-
-
-
-
-
 void drawMain()
 {
     initgraph(1400, 720);
@@ -50,43 +39,6 @@ string getInput(string line)
     InputBox(s, 10, lpctstr);
     string lineback = s;
     return lineback;
-}
-
-void changepcb(PCB process[])
-{
-    
-    for (int i = 0; i < maxprocess; i++)
-    {
-        process[i].pid = -1;
-    }
-
-    for (int i = 0; i < 3; i++)
-    {
-        process[i].pid = i;
-        process[i].state = READY;
-        process[i].size = 128;
-    }
-
-    for (int i = 3; i < 6; i++)
-    {
-        process[i].pid = i;
-        process[i].state = 2;
-        process[i].size = 128;
-    }
-
-    for (int i = 6; i < 10; i++)
-    {
-        process[i].pid = i;
-        process[i].state = 3;
-        process[i].size = 128;
-
-        process[i].arrivetime=0; // 进程到达时间
-        process[i].needtime=1; // 进程总共需要运行的时间
-        process[i].remaintime=2; // 进程还需运行的时间
-        process[i].finishtime=3; // 进程运行结束的时间
-    }
-
-    
 }
 
 void drawCreatePCB()
@@ -173,7 +125,7 @@ void pcbDrawPro(PCB process[])
     for (size_t i = 0; i < maxprocess; i++)
     {
         if (process[i].pid >= 0)
-            if(process[i].state != 4)
+            if (process[i].state != 4)
                 truesize += 1;
         if (truesize >= 10)
             break;
@@ -185,21 +137,21 @@ void pcbDrawPro(PCB process[])
     settextcolor(WHITE);// 创建绘图窗口
 
 
-    
+
 
     char s[2000] = "";
     string w = (string)s;
 
     //InputBox(s, 10, "请输入半径");
-    
+
 
     printf("%s", s);
 
-    int x , y , z , m , T=180;
+    int x, y, z, m, T = 180;
     COLORREF colorThis;
     int processptr = 0;
     strcpy(s, "进程");
-    outtextxy( 20, 150, s);
+    outtextxy(20, 150, s);
 
     strcpy(s, "状态");
     outtextxy(20, 220, s);
@@ -209,7 +161,7 @@ void pcbDrawPro(PCB process[])
 
     strcpy(s, "进程到达时间");
     outtextxy(20, 340, s);
-    
+
     strcpy(s, "总计运行时间");
     outtextxy(20, 400, s);
 
@@ -221,10 +173,10 @@ void pcbDrawPro(PCB process[])
 
     for (int i = 0; i < Ready_Process.size(); i++)
     {
-        outtextxy(20+ i * 40, 570, Ready_Process.front());
+        outtextxy(20 + i * 40, 570, Ready_Process.front());
     }
 
-    
+
     int arrivetime; // 进程到达时间
     int needtime; // 进程总共需要运行的时间
     int remaintime; // 进程还需运行的时间
@@ -236,9 +188,9 @@ void pcbDrawPro(PCB process[])
     {
         while (true)
         {
-            if(process[processptr].state != 4)
+            if (process[processptr].state != 4)
             {
-                
+
                 break;
 
             }
@@ -253,26 +205,26 @@ void pcbDrawPro(PCB process[])
         }
 
 
-        
-        x = 0 + i * 1200 / truesize, y = 120, z = x + 100 , m = y+60;
+
+        x = 0 + i * 1200 / truesize, y = 120, z = x + 100, m = y + 60;
         settextcolor(WHITE);
         _itoa(process[processptr].pid, s, 10);
         outtextxy(x + 120, y - 20, s);
-        
+
         switch (process[processptr].state)
         {
-            case 1:strcpy(s, "就绪"); break;
-            case 2:strcpy(s, "运行"); break;
-            case 3:strcpy(s, "阻塞"); break;
-            default:break;
+        case 1:strcpy(s, "就绪"); break;
+        case 2:strcpy(s, "运行"); break;
+        case 3:strcpy(s, "阻塞"); break;
+        default:break;
         }
         outtextxy(x + 120, m + 40, s);
 
         _itoa(process[processptr].size, s, 10);
-        outtextxy(x + 120, m + 40 +60, s);
+        outtextxy(x + 120, m + 40 + 60, s);
 
         setfillcolor(colorThis);
-        
+
         // 进程到达时间
         _itoa(process[processptr].arrivetime, s, 10);
         outtextxy(x + 120, m + 40 + 120, s);
@@ -286,7 +238,7 @@ void pcbDrawPro(PCB process[])
         _itoa(process[processptr].finishtime, s, 10);
         outtextxy(x + 120, m + 40 + 300, s);
 
-        
+
         fillellipse(x + 120, y, z + 120, m);	// 画圆，圆心(200, 200)，半径 100
 
 
@@ -294,8 +246,8 @@ void pcbDrawPro(PCB process[])
     }
 
     int button_x = 50, button_y = 20;
-    ellipse(button_x, button_y, button_x+100, button_y+50);
-    outtextxy(button_x+20, button_y+20, "创建进程");
+    ellipse(button_x, button_y, button_x + 100, button_y + 50);
+    outtextxy(button_x + 20, button_y + 20, "创建进程");
 
     button_x = 1050;
     ellipse(button_x, button_y, button_x + 100, button_y + 50);
@@ -321,7 +273,7 @@ void pcbDrawPro(PCB process[])
                 return;
 
             }
-            else if(me.x >= button_x+ 1000 && me.y >= button_y && me.x <= button_x + 1000 + 100 && me.y <= button_y + 60)
+            else if (me.x >= button_x + 1000 && me.y >= button_y && me.x <= button_x + 1000 + 100 && me.y <= button_y + 60)
             {
                 //时间前进
                 pcbDrawPro(process);
@@ -341,29 +293,6 @@ void pcbDrawPro(PCB process[])
 
     return;
 
-}
-
-void InitializeMemory()
-{
-    int i, j;//循环变量
-    //初始化虚拟内存
-
-    for (i = 0; i < PAGE_NUMBER; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (j == 0) virMemory.virTable[i][j] = -1;
-            if (j == 1) virMemory.virTable[i][j] = 0;
-            if (j == 2) virMemory.virTable[i][j] = 0;
-        }
-        virMemory.virContent[i] = "*";
-    }
-    //初始化物理内存
-    for (i = 0; i < FRAME_NUMBER; i++)
-    {
-        phyMemory.phyTable[i] = -1;
-        phyMemory.phyContent[i] = "*";
-    }
 }
 
 void drawVirMemory()
@@ -390,15 +319,15 @@ void drawVirMemory()
     outtextxy(100 + 20, 100 + 20, "页号");
     for (int i = 0; i < PAGE_NUMBER; i++)
     {
-        rectangle( 200 + i * 100, 90,  300 + i * 100,190 );
+        rectangle(200 + i * 100, 90, 300 + i * 100, 190);
         _itoa(i, s, 10);
-        outtextxy(200 + i * 100 + 20, 100  + 20, s);
+        outtextxy(200 + i * 100 + 20, 100 + 20, s);
         for (int j = 0; j < 3; j++)
         {
-            int table_x = 200 + i*100, table_y = 200 + j * 100;
+            int table_x = 200 + i * 100, table_y = 200 + j * 100;
             rectangle(table_x, table_y, table_x + 100, table_y + 100);
             _itoa(virMemory.virTable[i][j], s, 10);
-            outtextxy(table_x+20, table_y+20, s);
+            outtextxy(table_x + 20, table_y + 20, s);
         }
 
     }
@@ -431,11 +360,11 @@ void drawPhyMemory()
         rectangle(200 + i * 100, 90, 300 + i * 100, 190);
         _itoa(i, s, 10);
         outtextxy(200 + i * 100 + 20, 100 + 20, s);
-        int table_x = 200 + i * 100, table_y = 200 ;
+        int table_x = 200 + i * 100, table_y = 200;
         rectangle(table_x, table_y, table_x + 100, table_y + 100);
         _itoa(phyMemory.phyTable[i], s, 10);
         outtextxy(table_x + 20, table_y + 20, s);
-        
+
 
     }
 }
@@ -496,7 +425,7 @@ void memoryDraw()
         me = getmessage(EM_MOUSE);//获取鼠标消息
         if (me.message == WM_LBUTTONUP)
         {
-            
+
             button_x = 100, button_y = 600;
             if (me.x >= button_x && me.y >= button_y && me.x <= button_x + 100 && me.y <= button_y + 60)
             {
@@ -512,7 +441,7 @@ void memoryDraw()
             }
             else if (me.x >= button_x + 240 && me.y >= button_y && me.x <= button_x + 100 + 240 && me.y <= button_y + 60)
             {
-                
+
                 //return;
             }
             else if (me.x >= button_x + 1000 && me.y >= button_y && me.x <= button_x + 1000 + 100 && me.y <= button_y + 60)
@@ -544,89 +473,11 @@ void memoryDraw()
 }
 
 
-
-
-void init_directory()
-{
-    head = (FCB*)malloc(sizeof(FCB));
-    FCB* father1 = (FCB*)malloc(sizeof(FCB));
-    FCB* father2 = (FCB*)malloc(sizeof(FCB));
-    FCB* father3 = (FCB*)malloc(sizeof(FCB));
-    FCB* f1 = (FCB*)malloc(sizeof(FCB));
-    FCB* f2 = (FCB*)malloc(sizeof(FCB));
-    FCB* f3 = (FCB*)malloc(sizeof(FCB));
-    FCB* f4 = (FCB*)malloc(sizeof(FCB));
-    FCB* f5 = (FCB*)malloc(sizeof(FCB));
-    //初始化目录文件 
-
-    head->sonFCB = father1;
-    head->color = 1;
-    head->type = 1;
-    head->name = (char*)malloc(sizeof(char) * 20); // 分配内存
-    strcpy(head->name, "head");
-
-
-    father1->name = (char*)malloc(sizeof(char) * 20); // 分配内存
-    father1->color = 1;
-    strcpy(father1->name, "father1");
-    father1->type = 1;
-
-    father2->name = (char*)malloc(sizeof(char) * 20); // 分配内存
-    strcpy(father2->name, "father2");
-    father2->type = 1;
-
-    father3->name = (char*)malloc(sizeof(char) * 20); // 分配内存
-    strcpy(father3->name, "father3");
-    father3->type = 1;
-
-    f1->name = (char*)malloc(sizeof(char) * 20); // 分配内存
-    strcpy(f1->name, "f1");
-    f1->type = 0;
-
-    f2->name = (char*)malloc(sizeof(char) * 20); // 分配内存
-    strcpy(f2->name, "f2");
-    f2->type = 0;
-
-    f3->name = (char*)malloc(sizeof(char) * 20); // 分配内存
-    strcpy(f3->name, "f3");
-    f3->type = 0;
-
-    f4->name = (char*)malloc(sizeof(char) * 20); // 分配内存
-    strcpy(f4->name, "f4");
-    f4->type = 0;
-
-    f5->name = (char*)malloc(sizeof(char) * 20); // 分配内存
-    strcpy(f5->name, "f5");
-    f5->type = 0;
-
-
-    father1->nextFCB = father2;
-    father1->sonFCB = f1;
-    f1->color = 1;
-    f1->nextFCB = f2;
-    f2->nextFCB = f3;
-    father2->sonFCB = f4;
-    f4->nextFCB = father3;
-    father3->sonFCB = f5;
-
-    head->nextFCB = NULL;
-    f1->sonFCB = NULL;
-    f2->sonFCB = NULL;
-    f3->sonFCB = NULL;
-    f4->sonFCB = NULL;
-    f5->sonFCB = NULL;
-    father2->nextFCB = NULL;
-    father3->nextFCB = NULL;
-    f3->nextFCB = NULL;
-    f5->nextFCB = NULL;
-
-}
-
 int size1(FCB* a)
 {
-    FCB* b=a;
+    FCB* b = a;
     int num = 0;
-    while(b != NULL)
+    while (b != NULL)
     {
         b = b->nextFCB;
         num++;
@@ -659,7 +510,7 @@ void drawFileMenu(FCB* head)
             setfillcolor(RED);
         else
             setfillcolor(WHITE);
-        fillellipse(x, y , x + 60, y + 120);
+        fillellipse(x, y, x + 60, y + 120);
         outtextxy(x - 60, y + 80, temp->name);
         temp = temp->nextFCB;
 
@@ -681,8 +532,8 @@ void fileDraw()
     string w = (string)s;
     outtextxy(10, 20, s);
     int button_x = 20, button_y = 50;
-    ellipse(button_x, button_y, button_x+100, button_y+60);
-    outtextxy(button_x+30, button_y+20 , "根目录");
+    ellipse(button_x, button_y, button_x + 100, button_y + 60);
+    outtextxy(button_x + 30, button_y + 20, "根目录");
     button_x += 120;
 
 
@@ -694,7 +545,7 @@ void fileDraw()
     ellipse(button_x, button_y, button_x + 100, button_y + 50);
     outtextxy(button_x + 20, button_y + 20, "主界面");
 
-    int x=700, y=300 ;
+    int x = 700, y = 300;
     COLORREF colorThis;
     int processptr = 0;
     strcpy(s, "文件");
@@ -726,13 +577,13 @@ void fileDraw()
                 }
             }
             button_x = 20, button_y = 50;
-            if(me.x>= button_x &&me.y>= button_y &&me.x<= button_x +100&&me.y<= button_y+60)
+            if (me.x >= button_x && me.y >= button_y && me.x <= button_x + 100 && me.y <= button_y + 60)
             {
                 fileDraw();
                 return;
 
             }
-            else if (me.x >= button_x+1030 && me.y >= button_y && me.x <= button_x + 1030 + 100 && me.y <= button_y + 60)
+            else if (me.x >= button_x + 1030 && me.y >= button_y && me.x <= button_x + 1030 + 100 && me.y <= button_y + 60)
             {
                 //时间前进
                 fileDraw();
@@ -745,8 +596,8 @@ void fileDraw()
                 break;
 
             }
-                
-            
+
+
 
 
         }
@@ -754,40 +605,12 @@ void fileDraw()
 
     }
 
-    
+
     drawMain();
 
     return;
 
 }
-
-bool acquire_request(int pid, int device) {//device：请求设备号
-    for (int i = 0; i < Device_Table[device].size; i++) {
-        if (Device_Table[device].data[i] == pid) {
-            return false;//重复请求
-        }
-    }
-    Device_Table[device].data = (int*)realloc(Device_Table[device].data, (Device_Table[device].size + 1) * sizeof(int));
-    Device_Table[device].data[Device_Table[device].size] = pid;
-    Device_Table[device].size++;
-    if (Device_Table[device].size > Device_Table[device].num) {
-        return false; // 进程加入等待队列
-    }
-    else {
-        return true; //进程使用设备
-    }
-}
-
-void init_device() {
-    for (int i = 0; i < DeviceNum; i++) {
-        Device_Table[i].name = deviceName[i];
-        acquire_request(0, i);
-        acquire_request(1, i);
-        Device_Table[i].size = 2;
-        Device_Table[i].num = deviceNum[i];
-    }
-}
-
 
 void changedevice(PCB process[])
 {
@@ -835,7 +658,7 @@ void deviceDraw()
 
     for (int i = 0; i < DeviceNum; i++)
     {
-        if(Device_Table[i].size==0)
+        if (Device_Table[i].size == 0)
             colorThis = GREEN;
         else if (Device_Table[i].size > 0 && Device_Table[i].size <= Device_Table[i].num)
             colorThis = YELLOW;
@@ -861,7 +684,7 @@ void deviceDraw()
             outtextxy(x + 120, m + 40 + 60, s);
             m += 40;
         }
-        
+
         processptr++;
     }
 
@@ -918,7 +741,7 @@ int main()
     outtextxy(x, y - 20, q2);
     x += 100, z += 100;
     TCHAR q3[] = _T("内存");
-    fillrectangle(x, y, z,o);
+    fillrectangle(x, y, z, o);
     outtextxy(x, y - 20, q3);
     x += 100, z += 100;
     TCHAR q4[] = _T("进程");
@@ -928,11 +751,11 @@ int main()
     TCHAR q5[] = _T("退出");
     fillrectangle(x, y, z, o);
     outtextxy(x, y - 20, q5);
-    while (true) 
+    while (true)
     {
         ExMessage m;//定义一个消息变量
         m = getmessage(EM_MOUSE);//获取鼠标消息
-        if(m.message== WM_LBUTTONUP)
+        if (m.message == WM_LBUTTONUP)
         {
             if (m.x <= 100)
                 fileDraw();
@@ -946,8 +769,8 @@ int main()
                 break;
 
         }
-        
+
     }
-    
+
     return 0;
 }
