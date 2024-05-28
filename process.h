161e4d284d53e73e1523565b6f4baf7c4666ea18
InoRiix <166696559+InoRiix@users.noreply.github.com>
2024-05-28 20:41:42 +0800

    Update process.h

--- process.h
+++ process.h
@@ -1,5 +1,10 @@
 #pragma once
-#include<bits/stdc++.h>
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include<string>
+#include<stdbool.h>
+#include<queue>
 using namespace std;
 
 #define NOCMD 0
@@ -16,12 +21,12 @@ using namespace std;
 #define BLOCKED 3
 #define OVER 4
 
-struct CMD{
-    int commandtime;// 执行指令的时间
-    int commandid;//指令编号
-    int deviceid;//对象编号，如调用的设备，唤醒的进程等
+struct CMD {
+	int commandtime;// 执行指令的时间
+	int commandid;//指令编号
+	int deviceid;//对象编号，如调用的设备，唤醒的进程等
 	int devicetime;//使用设备的时间
-    string filename;//操作的文件名
+	string filename;//操作的文件名
 	string infile;//文件操作的字符串
 };
 
@@ -37,27 +42,27 @@ struct PCB {
 	CMD command; // 需要执行的指令
 };
 
-
-
-
-const int maxprocess=100;
-int timer=0;
-int totalP=0;
+const int maxprocess = 100;
+int timer = 0;
+int totalP = 0;
+int runningpid = -1;
 
 PCB process[maxprocess];
 vector <int> Done_Process;//存储已经结束的PCB信息
 queue <int> Ready_Process;//存储就绪态的PCB信息
 vector <int> Blocked_Process;//存储阻塞的PCB信息
 
-int createP(int processsize,int processtime,int ct,int cid,int did,
-			int dt,string fn,string inf);//创建进程，返回1创建成功，0失败
+int createP(int processsize, int processtime, int ct, int cid, int did,
+	int dt, string fn, string inf);//创建进程，返回1创建成功，0失败
 int blockP(int PID); //进程因为中断等问题阻塞，由运行态（或就绪态）变为阻塞态
 int wakeupP(int PID);//由阻塞态变为就绪态
 int suspendP(int PID);//使进程由运行态变为就绪态
 int runP(int PID);//使进程从就绪态变为运行态
 int runcmd(int PID, CMD cmd);//执行某个进程的一个指令
 int finishP(int PID);//进程运行结束
-int findnextprocess();
+int find_next_process();
 int FCFS();//使用FCFS算法进行进程调度
 void outPinfo(PCB p);
-void initialize_process();
+void init_process();
+void letsgo();
+void user_suspend();
